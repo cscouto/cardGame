39d3926e744ed99e2e4b597b57fbdc5c92d18c96
Couto <cs.couto@outlook.com>
2017-10-25 14:34:10 -0200

    [project] - sound/ fonts/ keycode

---
+++ CardGame/Utils/FontType.swift
@@ -0,0 +1,13 @@
+//
+//  FontType.swift
+//  CardGame
+//
+//  Created by Couto on 10/25/17.
+//  Copyright © 2017 coutocode. All rights reserved.
+//
+
+import Foundation
+
+enum FontType: String {
+    case HelveticaNeueLight = "HelveticaNeue-Light"
+}
---
+++ CardGame/Utils/SoundType.swift
@@ -0,0 +1,14 @@
+//
+//  SoundType.swift
+//  CardGame
+//
+//  Created by Couto on 10/25/17.
+//  Copyright © 2017 coutocode. All rights reserved.
+//
+
+enum SoundType: String {
+    case click = "Birdy_Note5.wav"
+    case peakClear = "Birdy_Notification1.wav"
+    case cardOpen = "cardSlide8.wav"
+    case cardSlide = "cardSlide1.wav"
+}
