86949c119725f7e5da350e2ec931a5f26d5943cc
Couto <cs.couto@outlook.com>
2017-10-25 14:38:50 -0200

    [project] - device type

--- CardGame/Utils/DeviceType.swift
+++ CardGame/Utils/DeviceType.swift
@@ -27,3 +27,51 @@
     }
     
 #endif
+
+enum BaseGameLayoutDevice: Int {
+    case iPhone4OrLess
+    case iPhone5
+    case iPhone6
+    case iPhone6p
+    case iPad
+}
+
+class BaseGameLayout: NSObject {
+    
+    class func setup(layout: BaseGameLayoutDevice) {
+        switch layout {
+        case .iPhone4OrLess:
+            self.setupIphone4OrLess()
+            break
+        case .iPhone5:
+            self.setupIphone5()
+            break
+        case .iPhone6:
+            self.setupIphone6()
+            break
+        case .iPhone6p:
+            self.setupIphone6p()
+            break
+        case .iPad:
+            self.setupIpad()
+            break
+        }
+    }
+    
+    class func setupIphone6() {
+        print("GameLayout - iPhone 6")
+    }
+    class func setupIphone6p() {
+        print("GameLayout - iPhone 6+")
+    }
+    class func setupIphone5() {
+        print("GameLayout - iPhone 5")
+    }
+    class func setupIphone4OrLess() {
+        print("GameLayout - <= iPhone 4")
+    }
+    class func setupIpad() {
+        print("GameLayout - iPad")
+    }
+    
+}
